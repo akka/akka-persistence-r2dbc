37418d856edea6296b11cf1228bc57a6de142ce8
Patrik Nordwall <patrik.nordwall@gmail.com>
2021-11-03 14:40:04 +0100

    eventsByPersistenceId query, #80
    * also verify that metadata from replicated event sourcing is included
      in eventsByPersistenceId and eventsBySlices

--- core/src/main/scala/akka/persistence/r2dbc/query/scaladsl/R2dbcReadJournal.scala
+++ core/src/main/scala/akka/persistence/r2dbc/query/scaladsl/R2dbcReadJournal.scala
@@ -5,6 +5,8 @@
 package akka.persistence.r2dbc.query.scaladsl
 
 import scala.collection.immutable
+import scala.concurrent.Future
+import scala.concurrent.duration.FiniteDuration
 
 import akka.NotUsed
 import akka.actor.ExtendedActorSystem
@@ -17,6 +19,7 @@ import akka.persistence.r2dbc.R2dbcSettings
 import akka.persistence.r2dbc.internal.BySliceQuery
 import akka.persistence.r2dbc.internal.ContinuousQuery
 import akka.persistence.r2dbc.internal.SliceUtils
+import akka.persistence.r2dbc.journal.JournalDao
 import akka.persistence.r2dbc.journal.JournalDao.SerializedJournalRow
 import akka.persistence.r2dbc.query.TimestampOffset
 import akka.serialization.SerializationExtension
@@ -27,6 +30,8 @@ import org.slf4j.LoggerFactory
 object R2dbcReadJournal {
   val Identifier = "akka.persistence.r2dbc.query"
 
+  private final case class ByPersistenceIdState(queryCount: Int, rowCount: Int, latestSeqNr: Long)
+
   private final case class PersistenceIdsQueryState(queryCount: Int, rowCount: Int, latestPid: String)
 }
 
@@ -34,7 +39,10 @@ final class R2dbcReadJournal(system: ExtendedActorSystem, config: Config, cfgPat
     extends ReadJournal
     with CurrentEventsBySliceQuery
     with EventsBySliceQuery
+    with CurrentEventsByPersistenceIdQuery
+    with EventsByPersistenceIdQuery
     with CurrentPersistenceIdsQuery {
+  import R2dbcReadJournal.ByPersistenceIdState
   import R2dbcReadJournal.PersistenceIdsQueryState
 
   private val log = LoggerFactory.getLogger(getClass)
@@ -44,12 +52,12 @@ final class R2dbcReadJournal(system: ExtendedActorSystem, config: Config, cfgPat
   import settings.maxNumberOfSlices
 
   private val typedSystem = system.toTyped
+  import typedSystem.executionContext
   private val serialization = SerializationExtension(system)
+  private val connectionFactory = ConnectionFactoryProvider(typedSystem)
+    .connectionFactoryFor(sharedConfigPath + ".connection-factory")
   private val queryDao =
-    new QueryDao(
-      settings,
-      ConnectionFactoryProvider(typedSystem)
-        .connectionFactoryFor(sharedConfigPath + ".connection-factory"))(typedSystem.executionContext, typedSystem)
+    new QueryDao(settings, connectionFactory)(typedSystem.executionContext, typedSystem)
 
   private val bySlice: BySliceQuery[SerializedJournalRow, EventEnvelope] = {
     val createEnvelope: (TimestampOffset, SerializedJournalRow) => EventEnvelope = (offset, row) => {
@@ -66,6 +74,8 @@ final class R2dbcReadJournal(system: ExtendedActorSystem, config: Config, cfgPat
 
     new BySliceQuery(queryDao, createEnvelope, extractOffset, settings, log)(typedSystem.executionContext)
   }
+
+  private val journalDao = new JournalDao(settings, connectionFactory)(typedSystem.executionContext, typedSystem)
 
   def extractEntityTypeHintFromPersistenceId(persistenceId: String): String =
     SliceUtils.extractEntityTypeHintFromPersistenceId(persistenceId)
@@ -91,6 +101,141 @@ final class R2dbcReadJournal(system: ExtendedActorSystem, config: Config, cfgPat
       maxSlice: Int,
       offset: Offset): Source[EventEnvelope, NotUsed] =
     bySlice.liveBySlices("eventsBySlices", entityTypeHint, minSlice, maxSlice, offset)
+
+  override def currentEventsByPersistenceId(
+      persistenceId: String,
+      fromSequenceNr: Long,
+      toSequenceNr: Long): Source[EventEnvelope, NotUsed] = {
+    def updateState(state: ByPersistenceIdState, row: SerializedJournalRow): ByPersistenceIdState =
+      state.copy(rowCount = state.rowCount + 1, latestSeqNr = row.sequenceNr)
+
+    def nextQuery(
+        state: ByPersistenceIdState,
+        highestSeqNr: Long): (ByPersistenceIdState, Option[Source[SerializedJournalRow, NotUsed]]) = {
+      if (state.queryCount == 0L || state.rowCount >= settings.querySettings.bufferSize) {
+        val newState = state.copy(rowCount = 0, queryCount = state.queryCount + 1)
+
+        if (state.queryCount != 0 && log.isDebugEnabled())
+          log.debug(
+            "currentEventsByPersistenceId query [{}] for persistenceId [{}], from [{}] to [{}]. Found [{}] rows in previous query.",
+            state.queryCount,
+            persistenceId,
+            state.latestSeqNr + 1,
+            highestSeqNr,
+            state.rowCount)
+
+        newState -> Some(
+          queryDao
+            .eventsByPersistenceId(persistenceId, state.latestSeqNr + 1, highestSeqNr))
+      } else {
+        log.debug(
+          "currentEventsByPersistenceId query [{}] for persistenceId [{}] completed. Found [{}] rows in previous query.",
+          state.queryCount,
+          persistenceId,
+          state.rowCount)
+
+        state -> None
+      }
+    }
+
+    val highestSeqNrFut =
+      if (toSequenceNr == Long.MaxValue) journalDao.readHighestSequenceNr(persistenceId, fromSequenceNr)
+      else Future.successful(toSequenceNr)
+
+    Source
+      .futureSource[SerializedJournalRow, NotUsed] {
+        highestSeqNrFut.map { highestSeqNr =>
+          if (log.isDebugEnabled())
+            log.debug(
+              "currentEventsByPersistenceId query for persistenceId [{}], from [{}] to [{}].",
+              persistenceId,
+              fromSequenceNr,
+              highestSeqNr)
+
+          ContinuousQuery[ByPersistenceIdState, SerializedJournalRow](
+            initialState = ByPersistenceIdState(0, 0, latestSeqNr = fromSequenceNr - 1),
+            updateState = updateState,
+            delayNextQuery = _ => None,
+            nextQuery = state => nextQuery(state, highestSeqNr))
+        }
+      }
+      .map(deserializeRow)
+      .mapMaterializedValue(_ => NotUsed)
+  }
+
+  override def eventsByPersistenceId(
+      persistenceId: String,
+      fromSequenceNr: Long,
+      toSequenceNr: Long): Source[EventEnvelope, NotUsed] = {
+    val someRefreshInterval = Some(settings.querySettings.refreshInterval)
+
+    log.debug("Starting eventsByPersistenceId query for persistenceId [{}], from [{}].", persistenceId, fromSequenceNr)
+
+    def nextOffset(state: ByPersistenceIdState, row: SerializedJournalRow): ByPersistenceIdState =
+      state.copy(rowCount = state.rowCount + 1, latestSeqNr = row.sequenceNr)
+
+    def delayNextQuery(state: ByPersistenceIdState): Option[FiniteDuration] = {
+      val delay =
+        if (0 <= state.rowCount && state.rowCount <= 1) someRefreshInterval
+        else None // immediately if there might be more rows to fetch
+
+      // TODO we could have different delays here depending on how many rows that were found.
+      // e.g. a short delay if rowCount is < some threshold
+
+      delay.foreach { d =>
+        log.debug(
+          "eventsByPersistenceId query [{}] for persistenceId [{}] delay next [{}] ms.",
+          state.queryCount,
+          persistenceId,
+          d.toMillis)
+      }
+
+      delay
+    }
+
+    def nextQuery(
+        state: ByPersistenceIdState): (ByPersistenceIdState, Option[Source[SerializedJournalRow, NotUsed]]) = {
+      if (state.latestSeqNr >= toSequenceNr) {
+        log.debug(
+          "eventsByPersistenceId query [{}] for persistenceId [{}] completed. Found [{}] rows in previous query.",
+          state.queryCount,
+          persistenceId,
+          state.rowCount)
+        state -> None
+      } else {
+        val newState = state.copy(rowCount = 0, queryCount = state.queryCount + 1)
+
+        log.debug(
+          "eventsByPersistenceId query [{}] for persistenceId [{}], from [{}]. Found [{}] rows in previous query.",
+          newState.queryCount,
+          persistenceId,
+          state.rowCount)
+
+        newState ->
+        Some(
+          queryDao
+            .eventsByPersistenceId(persistenceId, state.latestSeqNr + 1, toSequenceNr))
+      }
+    }
+
+    ContinuousQuery[ByPersistenceIdState, SerializedJournalRow](
+      initialState = ByPersistenceIdState(0, 0, latestSeqNr = fromSequenceNr - 1),
+      updateState = nextOffset,
+      delayNextQuery = delayNextQuery,
+      nextQuery = nextQuery)
+      .map(deserializeRow)
+  }
+
+  def deserializeRow(row: SerializedJournalRow): EventEnvelope = {
+    val payload = serialization.deserialize(row.payload, row.serId, row.serManifest).get
+    val offset = TimestampOffset(row.dbTimestamp, row.readDbTimestamp, Map(row.persistenceId -> row.sequenceNr))
+    val envelope = EventEnvelope(offset, row.persistenceId, row.sequenceNr, payload, row.timestamp)
+    row.metadata match {
+      case None => envelope
+      case Some(meta) =>
+        envelope.withMetadata(serialization.deserialize(meta.payload, meta.serId, meta.serManifest).get)
+    }
+  }
 
   def currentPersistenceIds(afterId: Option[String], limit: Long): Source[String, NotUsed] =
     queryDao.persistenceIds(afterId, limit)
@@ -132,4 +277,5 @@ final class R2dbcReadJournal(system: ExtendedActorSystem, config: Config, cfgPat
       nextQuery = state => nextQuery(state))
       .mapMaterializedValue(_ => NotUsed)
   }
+
 }
---
+++ core/src/test/scala/akka/persistence/r2dbc/query/EventsByPersistenceIdSpec.scala
@@ -0,0 +1,222 @@
+/*
+ * Copyright (C) 2021 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.persistence.r2dbc.query
+
+import akka.Done
+import akka.NotUsed
+import akka.actor.testkit.typed.scaladsl.LogCapturing
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.actor.typed.ActorSystem
+import akka.persistence.query.EventEnvelope
+import akka.persistence.query.PersistenceQuery
+import akka.persistence.r2dbc.R2dbcSettings
+import akka.persistence.r2dbc.TestActors
+import akka.persistence.r2dbc.TestActors.Persister
+import akka.persistence.r2dbc.TestActors.Persister.PersistWithAck
+import akka.persistence.r2dbc.TestConfig
+import akka.persistence.r2dbc.TestData
+import akka.persistence.r2dbc.TestDbLifecycle
+import akka.persistence.r2dbc.query.scaladsl.R2dbcReadJournal
+import akka.persistence.typed.PersistenceId
+import akka.persistence.typed.ReplicaId
+import akka.persistence.typed.ReplicationId
+import akka.persistence.typed.internal.ReplicatedEventMetadata
+import akka.persistence.typed.scaladsl.ReplicatedEventSourcing
+import akka.stream.scaladsl.Source
+import akka.stream.testkit.TestSubscriber
+import akka.stream.testkit.scaladsl.TestSink
+import org.scalatest.wordspec.AnyWordSpecLike
+
+object EventsByPersistenceIdSpec {
+  sealed trait QueryType
+  case object Live extends QueryType
+  case object Current extends QueryType
+}
+
+class EventsByPersistenceIdSpec
+    extends ScalaTestWithActorTestKit(TestConfig.config)
+    with AnyWordSpecLike
+    with TestDbLifecycle
+    with TestData
+    with LogCapturing {
+  import EventsByPersistenceIdSpec._
+
+  override def typedSystem: ActorSystem[_] = system
+  private val settings = new R2dbcSettings(system.settings.config.getConfig("akka.persistence.r2dbc"))
+
+  private val query = PersistenceQuery(testKit.system).readJournalFor[R2dbcReadJournal](R2dbcReadJournal.Identifier)
+
+  List[QueryType](Live, Current).foreach { queryType =>
+    def doQuery(pid: String, from: Long, to: Long): Source[EventEnvelope, NotUsed] =
+      queryType match {
+        case Live =>
+          query.eventsByPersistenceId(pid, from, to)
+        case Current =>
+          query.currentEventsByPersistenceId(pid, from, to)
+      }
+
+    def assertFinished(probe: TestSubscriber.Probe[_], liveShouldFinish: Boolean = false): Unit =
+      queryType match {
+        case Live if !liveShouldFinish =>
+          probe.expectNoMessage()
+          probe.cancel()
+        case _ =>
+          probe.expectComplete()
+      }
+
+    s"$queryType eventsByPersistenceId" should {
+      "populates timestamp offset" in {
+        val pid = nextPid()
+        val persister = testKit.spawn(Persister(pid))
+        val probe = testKit.createTestProbe[Done]()
+        persister ! Persister.PersistWithAck("e-1", probe.ref)
+        probe.expectMessage(Done)
+
+        val sub = doQuery(pid, 0, Long.MaxValue)
+          .runWith(TestSink())
+          .request(1)
+
+        sub.expectNextPF {
+          case EventEnvelope(TimestampOffset(_, _, seen), `pid`, 1, "e-1") if seen == Map(pid -> 1) =>
+        }
+
+        assertFinished(sub)
+      }
+
+      "return all events then complete" in {
+        val pid = nextPid()
+        val persister = testKit.spawn(Persister(pid))
+        val probe = testKit.createTestProbe[Done]()
+        val events = (1 to 20).map { i =>
+          val payload = s"e-$i"
+          persister ! PersistWithAck(payload, probe.ref)
+          probe.expectMessage(Done)
+          payload
+        }
+
+        val sub = doQuery(pid, 0, Long.MaxValue)
+          .map(_.event)
+          .runWith(TestSink())
+
+        sub
+          .request(events.size + 1)
+          .expectNextN(events.size)
+
+        assertFinished(sub)
+      }
+
+      "only return sequence nrs requested" in {
+        val pid = nextPid()
+        val persister = testKit.spawn(Persister(pid))
+        val probe = testKit.createTestProbe[Done]()
+        val events = (1 to 20).map { i =>
+          val payload = s"e-$i"
+          persister ! PersistWithAck(payload, probe.ref)
+          probe.expectMessage(Done)
+          payload
+        }
+
+        val sub = doQuery(pid, 0, 5)
+          .map(_.event)
+          .runWith(TestSink())
+
+        sub
+          .request(events.size + 1)
+          .expectNextN(events.take(5))
+
+        assertFinished(sub, liveShouldFinish = true)
+      }
+
+      "allow querying for a single event" in {
+        val pid = nextPid()
+        val persister = testKit.spawn(Persister(pid))
+        val probe = testKit.createTestProbe[Done]()
+
+        (1 to 3).map { i =>
+          val payload = s"e-$i"
+          persister ! PersistWithAck(payload, probe.ref)
+          probe.expectMessage(Done)
+          payload
+        }
+
+        val sub = doQuery(pid, 2, 2)
+          .map(_.event)
+          .runWith(TestSink())
+
+        val event = sub
+          .request(2)
+          .expectNext()
+        event should ===("e-2")
+
+        assertFinished(sub, liveShouldFinish = true)
+      }
+
+      "include metadata" in {
+        val probe = testKit.createTestProbe[Done]()
+        val entityType = nextEntityTypeHint()
+        val entityId = "entity-1"
+
+        val persister = testKit.spawn(TestActors.replicatedEventSourcedPersister(entityType, entityId))
+        persister ! Persister.PersistWithAck("e-1", probe.ref)
+        probe.expectMessage(Done)
+        persister ! Persister.PersistWithAck("e-2", probe.ref)
+        probe.expectMessage(Done)
+
+        val sub = doQuery(PersistenceId(entityType, entityId).id, 0, Long.MaxValue)
+          .runWith(TestSink())
+          .request(10)
+
+        val env1 = sub.expectNext()
+        env1.event shouldBe "e-1"
+        val meta1 = env1.eventMetadata.get.asInstanceOf[ReplicatedEventMetadata]
+        meta1.originReplica.id shouldBe "dc-1"
+        meta1.originSequenceNr shouldBe 1L
+
+        val env2 = sub.expectNext()
+        env2.event shouldBe "e-2"
+        val meta2 = env2.eventMetadata.get.asInstanceOf[ReplicatedEventMetadata]
+        meta2.originReplica.id shouldBe "dc-1"
+        meta2.originSequenceNr shouldBe 2L
+
+        assertFinished(sub)
+      }
+    }
+  }
+
+  "Live query" should {
+    "pick up new events" in {
+      val pid = nextPid
+      val persister = testKit.spawn(Persister(pid))
+      val probe = testKit.createTestProbe[Done]()
+      val sub = query
+        .eventsByPersistenceId(pid, 0, Long.MaxValue)
+        .map(_.event)
+        .runWith(TestSink())
+      val events = (1 to 20).map { i =>
+        val payload = s"e-$i"
+        persister ! PersistWithAck(payload, probe.ref)
+        probe.expectMessage(Done)
+        payload
+      }
+
+      sub.request(21)
+      sub.expectNextN(events)
+
+      val events2 = (21 to 40).map { i =>
+        val payload = s"e-$i"
+        // make the live query can deliver an element it picks up so it can end its query and give up the sesion
+        sub.request(1)
+        persister ! PersistWithAck(payload, probe.ref)
+        probe.expectMessage(Done)
+        payload
+      }
+      sub.request(1)
+      sub.expectNextN(events2)
+
+      sub.expectNoMessage()
+      sub.cancel()
+    }
+  }
+}
