19ff7291a6cdd0151052d71f2a3cc66dc1406ebd
Patrik Nordwall <patrik.nordwall@gmail.com>
2023-06-22 13:05:27 +0200

    javadsl

--- core/src/main/scala/akka/persistence/r2dbc/query/javadsl/R2dbcReadJournal.scala
+++ core/src/main/scala/akka/persistence/r2dbc/query/javadsl/R2dbcReadJournal.scala
@@ -55,12 +55,86 @@ final class R2dbcReadJournal(delegate: scaladsl.R2dbcReadJournal)
       offset: Offset): Source[EventEnvelope[Event], NotUsed] =
     delegate.currentEventsBySlices(entityType, minSlice, maxSlice, offset).asJava
 
+  /**
+   * Query events for given slices. A slice is deterministically defined based on the persistence id. The purpose is to
+   * evenly distribute all persistence ids over the slices.
+   *
+   * The consumer can keep track of its current position in the event stream by storing the `offset` and restart the
+   * query from a given `offset` after a crash/restart.
+   *
+   * The supported offset is [[akka.persistence.query.TimestampOffset]] and [[Offset.noOffset]].
+   *
+   * The timestamp is based on the database `CURRENT_TIMESTAMP` when the event was stored. `CURRENT_TIMESTAMP` is the
+   * time when the transaction started, not when it was committed. This means that a "later" event may be visible first
+   * and when retrieving events after the previously seen timestamp we may miss some events. In distributed SQL
+   * databases there can also be clock skews for the database timestamps. For that reason it will perform additional
+   * backtracking queries to catch missed events. Events from backtracking will typically be duplicates of previously
+   * emitted events. It's the responsibility of the consumer to filter duplicates and make sure that events are
+   * processed in exact sequence number order for each persistence id. Such deduplication is provided by the R2DBC
+   * Projection.
+   *
+   * Events emitted by the backtracking don't contain the event payload (`EventBySliceEnvelope.event` is None) and the
+   * consumer can load the full `EventBySliceEnvelope` with [[R2dbcReadJournal.loadEnvelope]].
+   *
+   * The events will be emitted in the timestamp order with the caveat of duplicate events as described above. Events
+   * with the same timestamp are ordered by sequence number.
+   *
+   * The stream is not completed when it reaches the end of the currently stored events, but it continues to push new
+   * events when new events are persisted. Corresponding query that is completed when it reaches the end of the
+   * currently stored events is provided by [[R2dbcReadJournal.currentEventsBySlices]].
+   */
   override def eventsBySlices[Event](
       entityType: String,
       minSlice: Int,
       maxSlice: Int,
       offset: Offset): Source[EventEnvelope[Event], NotUsed] =
     delegate.eventsBySlices(entityType, minSlice, maxSlice, offset).asJava
+
+  /**
+   * Same as `currentEventsBySlices` but with the purpose to use snapshots as starting points and thereby reducing
+   * number of events that have to be loaded. This can be useful if the consumer start from zero without any previously
+   * processed offset or if it has been disconnected for a long while and its offset is far behind.
+   *
+   * First it loads all snapshots with timestamps greater than or equal to the offset timestamp. There is at most one
+   * snapshot per persistenceId. The snapshots are transformed to events with the given `transformSnapshot` function.
+   *
+   * After emitting the snapshot events the ordinary events with sequence numbers after the snapshots are emitted.
+   *
+   * To use `currentEventsBySlicesStartingFromSnapshots` you must enable configuration
+   * `akka.persistence.r2dbc.query.start-from-snapshot.enabled` and follow instructions in migration guide
+   * https://doc.akka.io/docs/akka-persistence-r2dbc/current/migration.html#eventsBySlicesStartingFromSnapshots
+   */
+  def currentEventsBySlicesStartingFromSnapshots[Snapshot, Event](
+      entityType: String,
+      minSlice: Int,
+      maxSlice: Int,
+      offset: Offset,
+      transformSnapshot: java.util.function.Function[Snapshot, Event]): Source[EventEnvelope[Event], NotUsed] =
+    delegate
+      .currentEventsBySlicesStartingFromSnapshots(entityType, minSlice, maxSlice, offset, transformSnapshot(_))
+      .asJava
+
+  /**
+   * Same as `eventsBySlices` but with the purpose to use snapshots as starting points and thereby reducing number of
+   * events that have to be loaded. This can be useful if the consumer start from zero without any previously processed
+   * offset or if it has been disconnected for a long while and its offset is far behind.
+   *
+   * First it loads all snapshots with timestamps greater than or equal to the offset timestamp. There is at most one
+   * snapshot per persistenceId. The snapshots are transformed to events with the given `transformSnapshot` function.
+   *
+   * After emitting the snapshot events the ordinary events with sequence numbers after the snapshots are emitted.
+   *
+   * To use `eventsBySlicesStartingFromSnapshots` you must enable configuration
+   * `akka.persistence.r2dbc.query.start-from-snapshot.enabled` and follow instructions in migration guide
+   * https://doc.akka.io/docs/akka-persistence-r2dbc/current/migration.html#eventsBySlicesStartingFromSnapshots
+   */
+  def eventsBySlicesStartingFromSnapshots[Snapshot, Event](
+      entityType: String,
+      minSlice: Int,
+      maxSlice: Int,
+      offset: Offset,
+      transformSnapshot: java.util.function.Function[Snapshot, Event]): Source[EventEnvelope[Event], NotUsed] =
+    delegate.eventsBySlicesStartingFromSnapshots(entityType, minSlice, maxSlice, offset, transformSnapshot(_)).asJava
 
   override def sliceRanges(numberOfRanges: Int): util.List[Pair[Integer, Integer]] = {
     import akka.util.ccompat.JavaConverters._
