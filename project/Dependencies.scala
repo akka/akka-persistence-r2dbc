43fc9bece6f3f0dc91d30d0041e9b430e9d4ea61
Patrik Nordwall <patrik.nordwall@gmail.com>
2023-09-15 15:44:33 +0200

    bump: Scala 3.3.1 and 2.13.12 (#457)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,9 +5,9 @@
 import sbt._
 
 object Dependencies {
-  val Scala213 = "2.13.11"
-  val Scala212 = "2.12.17"
-  val Scala3 = "3.2.2"
+  val Scala213 = "2.13.12"
+  val Scala212 = "2.12.18"
+  val Scala3 = "3.3.1"
   val Scala2Versions = Seq(Scala213, Scala212)
   val ScalaVersions = Dependencies.Scala2Versions :+ Dependencies.Scala3
   val AkkaVersion = System.getProperty("override.akka.version", "2.9.0-M1")
